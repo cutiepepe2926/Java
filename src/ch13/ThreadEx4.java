4574d4e8fa10c58a46df41a904882898038c0c56
cutiepepe29266 <cutiepepe2926@gmail.com>
2025-10-09 16:50:04 +0900

    1009-First

---
+++ src/ch13/ThreadEx4.java
@@ -0,0 +1,20 @@
+package ch13;
+
+public class ThreadEx4 {
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+
+        for(int i=0;i<300;i++){
+            System.out.printf("%s", new String("-"));
+        }
+        System.out.println();
+        System.out.printf("소요시간1: "+(System.currentTimeMillis()-startTime));
+        System.out.println();
+
+        for(int i=0;i<300;i++){
+            System.out.printf("%s", new String("|"));
+        }
+        System.out.println();
+        System.out.printf("소요시간2: "+(System.currentTimeMillis()-startTime));
+    }
+}
